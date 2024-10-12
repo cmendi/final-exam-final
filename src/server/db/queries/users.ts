eeadb652d5352e9d6a956f349085e9fb12150fb3
mendi <mendenhall_chris1246@yahoo.com>
2024-10-12 12:56:38 -0700

    fixed registration issue

--- src/server/db/queries/users.ts
+++ src/server/db/queries/users.ts
@@ -2,7 +2,7 @@ import { Query } from "../connections";
 import { User } from "../../types";
 
 const find = (column: string, email: string) => Query<User[]>("SELECT * FROM Users WHERE ?? = ?", [column, email]);
-const insert = (newUser: { email: string; password: string }) => Query("INSERT INTO Users SET = ?", [newUser]);
+const insert = (newUser: { email: string; password: string }) => Query("INSERT INTO Users SET ?", [newUser]);
 
 export default {
 	find,
